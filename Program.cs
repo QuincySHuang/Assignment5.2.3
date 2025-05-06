e40c943dba3a40daaed561ca87009259f74d39e0
Quincy <qsyhuang@gmail.com>
2025-05-06 15:38:02 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Enter word to check if palindrome");
+string input = Console.ReadLine();
+
+input = input.ToLower();
+
+if (Palindrome(input,0, input.Length - 1))
+{
+    Console.WriteLine($"{input } is palindrome");
+}
+else
+{
+    Console.WriteLine($"{input} is not a palindrome");
+}
+
+
+bool Palindrome(string s, int beginning, int end)
+{
+    if(beginning >= end)
+    {
+        return true;
+    }
+    if (s[beginning] != s[end])
+    {
+        return false;
+    }
+
+    return Palindrome(s, beginning + 1, end - 1);
+
+}
